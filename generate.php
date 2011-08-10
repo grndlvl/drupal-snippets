ef06a5de817749bd06f8ce430f13509132531ee4
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2011-08-10 18:11:12 -0400

    adding copyright info

--- generate.php
+++ generate.php
@@ -1,4 +1,21 @@
 <?php
+/*
+  This is a script that will create snipMate snippets for Drupal to be used with Vim.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 if (!isset($_SERVER['argv'][1])) {
   print "Usage: php generate.php [path to drupal]\n"; exit;
 }
